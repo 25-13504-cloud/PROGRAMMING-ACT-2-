ea61febf14bcd2beaf173466ab0d3ba1e30a5467
25-13504-cloud <25-13504@g.batstate-u.edu.ph>
2026-09-11 11:12:37 +0800

    Add name input and greeting to leap year script
    Added user name input and greeting.

--- act2_leap.py
+++ act2_leap.py
@@ -1,2 +1,9 @@
+# Name = Punzalan, Aljon E.
+# Section = BMET 2101
+# Task 3 = leap year test
+
+name = input("enter your name")
+print("hello, " + name)
+
 year = int(input("Enter a year: "))
 print("Is it a leap year?", (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0))
