556136eafacb21b68f9080390efe50b1e47418e1
25-13504-cloud <25-13504@g.batstate-u.edu.ph>
2026-09-11 11:14:48 +0800

    Update act2_temp.py

--- act2_temp.py
+++ act2_temp.py
@@ -1,3 +1,10 @@
+# Name = Punzalan, Aljon E.
+# Section = BMET 2101
+# Task 4 = temperature check
+
+name = input("enter your name")
+print("hello, " + name)
+
 celsius = float(input("Enter temperature in °C: "))
 fahrenheit = celsius * 9/5 + 32
 in_range = 20 <= celsius <= 30
